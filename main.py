dcd5a6310a6f478f1745aff9c337dbe777d772eb
elixc20 <elicombs2020@gmail.com>
2024-02-09 16:13:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def anagram(string1, string2):
+   if string1.isspace() == True or string2.isspace() == True:
+        return False
    string1 = string1.lower()
    string2 = string2.lower()
+   string1 = string1.split()
+   string2 = string2.split()
+   string1 = ''.join(string1)
+   string2 = ''.join(string2)
    if (len(string1) == len(string2)):
     new_string1 = sorted(string1)
     new_string2 = sorted(string2)
@@ -13,6 +19,6 @@ def anagram(string1, string2):
       return ('False')
 
 if __name__ == '__main__':
-    string1 = input()
-    string2 = input()
-    print(anagram(string1, string2))
+    word1 = input()
+    word2 = input()
+    print(anagram(word1, word2))
