a963797541654553b283c0237f95b8aab9c9bd0f
elixc20 <elicombs2020@gmail.com>
2024-02-08 18:20:33 -0600

    main.py
    Anagram

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 #REMOVE PASS AND FIX THIS FUNCTION
-def anagram():
-    pass
+def anagram(string1, string2):
+    list1 = list(string1)
+    list1.sort()
+    list2 = list(string2)
+    list2.sort()
 
-if __name__ == '__main__':
-    #REMOVE PASS YOUR CODE GOES HERE
-    pass
-    
+    if list1 == list2:
+        return ('True')
+    if list1 != list2:
+        return ('False')
+
+if __name__ == '__main__': 
+    input1 = str(input())
+    input2 = str(input())
+
+    list1 = []
+    list2 = []
+
+    print(anagram(list1, list2))
